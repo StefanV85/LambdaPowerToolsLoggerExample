4698dfc9c3dbbf57893678b248e16e6f849311d2
Stefan Volkmer <stefan@wakeword.de>
2022-06-01 14:36:41 +0200

    Test with nested objects

--- index.js
+++ index.js
@@ -2,14 +2,40 @@
 Object.defineProperty(exports, "__esModule", { value: true });
 const logger_1 = require("@aws-lambda-powertools/logger");
 const logger = new logger_1.Logger({ serviceName: "HelloWorld", logLevel: "DEBUG" });
+let myPersonObject = {
+    message: "This is the Message for Person Max",
+    personID: "123",
+    firstName: "Max",
+    familyName: "Musterman",
+    jobHistory: [
+        {
+            dateFrom: "2000-01-01",
+            dateTo: "2001-01-01",
+            jobID: 1,
+        },
+        {
+            dateFrom: "2001-01-01",
+            dateTo: "2002-01-01",
+            jobID: 2,
+        },
+        {
+            dateFrom: "2002-01-01",
+            dateTo: "2020-01-01",
+            jobID: 3,
+        },
+    ],
+};
+//@ts-ignore
+// logger.console = console;
 logger.debug("debug");
 logger.warn("warn");
 logger.error("error");
 logger.info("info");
+logger.info(myPersonObject);
 console.log("real logging");
 console.warn("real warning");
 console.info("real info");
-process.stdout.write("something");
-process.stdout.write("something");
-process.stdout.write("something");
+console.log(myPersonObject);
+process.stdout.write("std out");
+process.stdout.write("std out");
 //# sourceMappingURL=index.js.map
--- index.ts
+++ index.ts
@@ -1,13 +1,41 @@
 import { Logger } from "@aws-lambda-powertools/logger";
 
 const logger = new Logger({ serviceName: "HelloWorld", logLevel: "DEBUG" });
+let myPersonObject = {
+  message: "This is the Message for Person Max",
+  personID: "123",
+  firstName: "Max",
+  familyName: "Musterman",
+  jobHistory: [
+    {
+      dateFrom: "2000-01-01",
+      dateTo: "2001-01-01",
+      jobID: 1,
+    },
+    {
+      dateFrom: "2001-01-01",
+      dateTo: "2002-01-01",
+      jobID: 2,
+    },
+    {
+      dateFrom: "2002-01-01",
+      dateTo: "2020-01-01",
+      jobID: 3,
+    },
+  ],
+};
+//Just a Test
+//@ts-ignore
+// logger.console = console;
+
 logger.debug("debug");
 logger.warn("warn");
 logger.error("error");
 logger.info("info");
+logger.info(myPersonObject);
 console.log("real logging");
 console.warn("real warning");
 console.info("real info");
-process.stdout.write("something");
-process.stdout.write("something");
-process.stdout.write("something");
+console.log(myPersonObject); //With "outputCapture": "std" you only see "stringify" strings
+process.stdout.write("std out");
+process.stdout.write("std out");
